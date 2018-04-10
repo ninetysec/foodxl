05558008a8f6a82e481fc4c227c137058857b2e7
qfdk <qfdk2010@gmail.com>
2018-04-10 22:17:07 +0200

    [Dev] 自动填充地址

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 router.get('/', function (req, res, next) {
   res.json({
     code: 200,
     msg: 'api'
+  });
+});
+
+/**
+ * 地址自动填充
+ */
+router.get('/getAdresse/:adresse', function (req, res, next) {
+  var adresse = req.params.adresse;
+  var url = 'https://api-adresse.data.gouv.fr/search/?q=' + adresse + '&city=paris&type=street';
+  request.get(url, function (err, response, body) {
+    res.json(JSON.parse(body));
   });
 });
 
